a1b3ffdf43ca78f902774a7f3e095de013f3fd41
victhor409 <victhor409@gmail.com>
2021-01-09 00:41:31 -0300

    add home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div>
+      <>
      <Navbar/>
+     <Home/>
+     </>
     </div>
   );
 }
